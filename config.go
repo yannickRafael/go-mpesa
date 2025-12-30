6504cba3bb8e3ed14be6fb83598221f90857fde2
yannickRafael <yannickrafael286@gmail.com>
2025-12-30 18:24:24 +0200

    Add Config struct

---
+++ config.go
@@ -0,0 +1,19 @@
+package mpesa
+
+// Config holds the configuration parameters required by the M-Pesa API.
+type Config struct {
+	// APIHost is the hostname for the API (e.g., api.sandbox.vm.co.mz).
+	APIHost string
+	// APIKey is used for creating authorize transactions on the API.
+	APIKey string
+	// PublicKey is the Public Key for the M-Pesa API. Used for generating Authorization bearer tokens.
+	PublicKey string
+	// Origin is used for identifying the hostname which is sending transaction requests.
+	Origin string
+	// ServiceProviderCode is provided by Vodacom MZ.
+	ServiceProviderCode string
+	// InitiatorIdentifier is provided by Vodacom MZ.
+	InitiatorIdentifier string
+	// SecurityCredential is provided by Vodacom MZ.
+	SecurityCredential string
+}
